2e543c997eca5c8eb0944635ddc2377b035440b0
soboleva_e <elenasobolevaya@gmail.com>
2022-11-30 23:36:57 +0300

    add Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+
+
+const Profile = () => {
+  return (
+    <div className="row">
+      <div className="col-sm-3">
+        <img src="https://www.seekpng.com/png/full/356-3562377_personal-user.png" alt="" width={100} />
+      </div>
+      <div className="col-sm-9">
+        <h1>Фамилия и имя</h1>
+        <h2>Рассказ о себе</h2>
+        <p>E-mail: ivanov@ya.ru</p>
+        <p>Id: 2</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aspernatur voluptatum totam quo obcaecati modi magni quia. Animi eaque est aut aspernatur earum placeat? Aliquam illo deserunt iure eum nobis?
+        </p>
+      </div>
+    </div>
+
+  );
+};
+
+export default Profile;
