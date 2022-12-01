c3972efeafa8e635e87a4228e3bef4474a69df88
soboleva_e <elenasobolevaya@gmail.com>
2022-12-01 14:03:48 +0300

    function GetUser

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,19 @@
+import styles from './Profile.module.css';
 
-
-const Profile = () => {
+const Profile = (props) => {
+  let user = props.function();
+  // console.log(user);
   return (
     <div className="row">
       <div className="col-sm-3">
-        <img src="https://www.seekpng.com/png/full/356-3562377_personal-user.png" alt="" width={100} />
+        <img src={user.avatar} alt="" width={"100%"} />
       </div>
       <div className="col-sm-9">
-        <h1>Фамилия и имя</h1>
-        <h2>Рассказ о себе</h2>
-        <p>E-mail: ivanov@ya.ru</p>
-        <p>Id: 2</p>
-        <p>
+        <h1 style={{backgroundColor: "lightblue"}}>{user.name} {user.lastname}</h1>
+        <h2 className={styles.about}>{user.about}</h2>
+        <p>E-mail: {user.email}</p>
+        <p>Id: {user.id}</p>
+        <p className={styles.paragraph}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aspernatur voluptatum totam quo obcaecati modi magni quia. Animi eaque est aut aspernatur earum placeat? Aliquam illo deserunt iure eum nobis?
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { getUser } from './state';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
-    <App />
+    
+    <App function={getUser}/>
+
   </React.StrictMode>
   </BrowserRouter>
 );
